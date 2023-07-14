644b91168f742dad3585d77fba026c50966ab80b
DinhNguyen2004 <118755132+DinhNguyen2004@users.noreply.github.com>
2023-07-14 14:20:54 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'clustersgp.wkhy5e7.mongodb.net',
+    DB_USER: 'ngocdinh2004',
+    DB_PASS: 'Dinhqn123',
+    DB_DATABASE: 'shoppingonline',
+    EMAIL_USER: 'rapperqtv1@gmail.com', // Microsoft mail service
+    EMAIL_PASS: 'ydsdgbyufminfapv',
+    JWT_SECRET: 'Dinh',
+    JWT_EXPIRES: '36000000000000000000000000000000000000', // in milliseconds
+  };
+module.exports = MyConstants;
